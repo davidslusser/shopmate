4690034cdcde834ea657db7c6bfb250d6e7abecf
David Slusser <dbslusser@gmail.com>
2023-04-15 17:34:32 -0700

    template updates

--- django_project/storemgr/reports.py
+++ django_project/storemgr/reports.py
@@ -1,12 +1,15 @@
+from django.conf import settings
+from django.shortcuts import render
+from django.views.generic import View
 from handyhelpers.views.report import AnnualTrendView, AnnualStatView, AnnualProgressView
+from handyhelpers.views.report import (get_colors, build_day_week_month_year_charts, get_timestamps)
 
 # import models
-from storemgr.models import Brand, Customer, Order, Product
+from storemgr.models import (Brand, Customer, Order, OrderStatus, Product)
 
 
 class StoreMgrAnnualProgressView(AnnualProgressView):
     """ """
-
     dataset_list = [
         dict(
             title="Brands",
@@ -41,7 +44,6 @@ class StoreMgrAnnualProgressView(AnnualProgressView):
 
 class StoreMgrAnnualStatView(AnnualStatView):
     """ """
-
     dataset_list = [
         dict(
             title="Brands",
@@ -76,7 +78,6 @@ class StoreMgrAnnualStatView(AnnualStatView):
 
 class StoreMgrAnnualTrendView(AnnualTrendView):
     """ """
-
     dataset_list = [
         dict(
             title="Brands",
@@ -107,3 +108,60 @@ class StoreMgrAnnualTrendView(AnnualTrendView):
             list_view="/storemgr/list_products",
         ),
     ]
+
+
+class StoreMgrDashboard(View):
+    """ """
+    base_template = getattr(settings, 'BASE_TEMPLATE', 'handyhelpers/handyhelpers_base.htm')
+    title = 'Annual Progress Report'
+    sub_title = 'cumulative data added over the past year'
+    template_name = 'storemgr/custom/dashboard.html'
+    
+    def get(self, request, *args, **kwargs):
+        """ """
+        context = {}
+        context['counts'] = [
+            {"title": "Brands", 
+             "count": Brand.objects.filter(enabled=True).count(), 
+             "icon": Brand.get_icon(), 
+             "link": "/storemgr/list_brands/?enabled=True"},
+            
+            {"title": "Customers", 
+             "count": Customer.objects.filter().count(), 
+             "icon": Customer.get_icon(), 
+             "link": "/storemgr/list_customers/"},
+            
+            {"title": "Products", 
+             "count": Product.objects.filter(enabled=True).count(), 
+             "icon": Product.get_icon(), 
+             "link": "/storemgr/list_products/?enabled=True"},
+            
+            {"title": "Orders", 
+             "count": Order.objects.filter().count(), 
+             "icon": Order.get_icon(), 
+             "link": "/storemgr/list_orders/"},
+            ]
+
+        # get Orders by brand
+        brand_list = [i.name for i in Brand.objects.all()]
+        context['orders_by_brand'] = dict(
+            id='orders_by_brand',
+            type='bar',
+            label_list=brand_list,
+            value_list=[Order.objects.filter(**{'products__brand__name': brand}).count() for brand in brand_list],
+            list_view='/storemgr/list_orders?products__brand__name=',
+            color_list=get_colors(len(brand_list)),
+        )
+        
+        # get Orders by status
+        status_list = [i.name for i in OrderStatus.objects.all()]
+        context['orders_by_status'] = dict(
+            id='orders_by_status',
+            type='pie',
+            label_list=status_list,
+            value_list=[Order.objects.filter(**{'status__name': status}).count() for status in status_list],
+            list_view='/storemgr/list_orders?status__name=',
+            color_list=get_colors(len(status_list)),
+        )
+                
+        return render(request, self.template_name, context=context)
--- django_project/storemgr/urls/gui.py
+++ django_project/storemgr/urls/gui.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("annual_progress/", reports.StoreMgrAnnualProgressView.as_view(), name="annual_progress"),
     path("annual_stats/", reports.StoreMgrAnnualStatView.as_view(), name="annual_stats"),
     path("annual_trends/", reports.StoreMgrAnnualTrendView.as_view(), name="annual_trends"),
+    path("dashboard/", reports.StoreMgrDashboard.as_view(), name="dashboard"),
     path("list_brands/", gui.ListBrands.as_view(), name="list_brands"),
     path("list_customers/", gui.ListCustomers.as_view(), name="list_customers"),
     path("list_orders/", gui.ListOrders.as_view(), name="list_orders"),
